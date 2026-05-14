910c91b0e4ac7bc823d48d3d6b7a24e62821b094
Ruggero Nocera <9341160+rogierz@users.noreply.github.com>
2026-05-14 21:17:08 +0100

    astro

--- src/locales/locale.ts
+++
@@ -1,7 +0,0 @@
-export const locale = {
-    "en": {
-
-    },
-    "it": {
-    }
-}
--- src/navigation.ts
+++ src/navigation.ts
@@ -1,122 +1,34 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
     {
-      text: 'Homes',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-      ],
+      text: 'Home',
+      href: getPermalink('/'),
     },
-    {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    }
+    /*{
+      text: 'Chi sono',
+      href: getPermalink('/about'),
+    },*/
   ],
-  actions: [{
-        variant: 'secondary',
+  actions: [
+    {
+        variant: "secondary",
         text: 'Prenota una chiamata',
         href: 'https://calendar.app.google/U3pPf3jgJgWYUHdx5',
         target: '_blank',
         icon: 'tabler:calendar-plus',
-      }],
+      }
+    ]
 };
 
 export const footerData = {
-  links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
-  ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
+  links: [],
+  secondaryLinks: [],
   socialLinks: [
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/rogierz' },
   ],
   footNote: `
-    Made with ❤️ by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/rogierz"> Ruggero Nocera</a> · All rights reserved.
+    Made with ❤️ by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/rogierz"> Ruggero Nocera</a> · Tutti i diritti riservati.
   `,
 };
